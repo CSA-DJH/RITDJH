ed353c152fe4339a15da86b989d2179bfb06a64b
CSA-DJH <1865@chaparralstaracademy.com>
2016-10-31 12:45:04 -0500

    Python Chromebook Charging station
    Charging Station

--- TestLogger.py
+++ TestLogger.py
@@ -3,6 +3,7 @@ import csv
 import time
 import os
 import re
+import hashlib
 import RPi.GPIO as io 
 from datetime import datetime
 io.setmode(io.BCM) 
@@ -14,51 +15,58 @@ io.setup(power_pin, io.OUT)
 io.output(power_pin, False)
 PERIOD_OF_TIME = 1800
 def loginoffline():
-    f2 = open('UserListWithOutGradesWO10.csv - UserListWithOutGradesWO10.csv.csv', 'r')
-    f = open("Logins.txt","a") 
-    students=csv.reader(f2)
-    username=input("Please enter your username: ")
-    password=getpass.getpass("Please enter your password: ")
-    username_rowgetnumyo=2 #change host_row to the corresponding row - 1 (ie; row 45, put in 44) in google's csv
-    password_rowgetnum=3 #master_row to the schools student list
-    for hosts_rowyo in students:
-        row = 1
-        username=username.replace("@chaparralstaracademy.com","")
-        hosts_rowyo[username_rowgetnumyo]=hosts_rowyo[username_rowgetnumyo].replace("@chaparralstaracademy.com","")
-        hosts_rowyo[username_rowgetnumyo]=hosts_rowyo[username_rowgetnumyo].zfill(4)
-        if (username == hosts_rowyo[username_rowgetnumyo]) & (password == hosts_rowyo[password_rowgetnum]):
-            print("Logging in.", end=""),
-            time.sleep(1)
-            print(".", end=""),
-            time.sleep(1)
-            print(".")
-            time.sleep(3)
-            os.system("clear")
-            print("Logging in complete! Plug in your chromebook now;")
-            f.write(username+" "+str(datetime.now())+"\n")
-            f.close()
-            start = time.time()
-            while True :
-                io.output(power_pin, True)
-                
-                if time.time() > start + PERIOD_OF_TIME:
-                    print("POWER OFF")
-                    time.sleep(1)
-                    io.output(power_pin, False)
-                    time.sleep(3)
-                    loginoffline()
-                    break
-            break
-    print("Logging in.", end=""),
-    time.sleep(1)
-    print(".", end=""),
-    time.sleep(1)
-    print(".")
-    time.sleep(3)
-    os.system("clear")
-    print("Error logging in, please try again! ")
-    loginoffline()
-    f2.close()
-    f.close()
+    try:
+        f2 = open('hashd.csv', 'r')
+        f = open("Logins.txt","a") 
+        students=csv.reader(f2)
+        username=input("Please enter your username: ")
+        password=getpass.getpass("Please enter your password: ")
+        username_rowgetnumyo=2 #change host_row to the corresponding row - 1 (ie; row 45, put in 44) in google's csv
+        password_rowgetnum=3 #master_row to the schools student list
+        salt="gnuvie:^)"
+        for hosts_rowyo in students:
+            row = 1
+            username=username.replace("@chaparralstaracademy.com","")
+            hosts_rowyo[username_rowgetnumyo]=hosts_rowyo[username_rowgetnumyo].replace("@chaparralstaracademy.com","")
+            hosts_rowyo[username_rowgetnumyo]=hosts_rowyo[username_rowgetnumyo].zfill(4)
+            if(username=="displayport:^)"):
+                exit()
+            if (hashlib.md5((salt+username).encode("UTF-8")).hexdigest() == hosts_rowyo[username_rowgetnumyo]) & (hashlib.md5((salt+password).encode("UTF-8")).hexdigest() == hosts_rowyo[password_rowgetnum]):
+                print("Logging in.", end=""),
+                time.sleep(1)
+                print(".", end=""),
+                time.sleep(1)
+                print(".")
+                time.sleep(3)
+                os.system("clear")
+                print("Logging in complete! Plug in your chromebook now;")
+                f.write(username+" "+str(datetime.now())+"\n")
+                f.close()
+                start = time.time()
+                while True :
+                    io.output(power_pin, True)
+                    
+                    if time.time() > start + PERIOD_OF_TIME:
+                        print("POWER OFF")
+                        time.sleep(1)
+                        io.output(power_pin, False)
+                        time.sleep(3)
+                        loginoffline()
+                        break
+                break
+        print("Logging in.", end=""),
+        time.sleep(1)
+        print(".", end=""),
+        time.sleep(1)
+        print(".")
+        time.sleep(3)
+        os.system("clear")
+        print("Error logging in, please try again! ")
+        loginoffline()
+        f2.close()
+        f.close()
+    except KeyboardInterrupt:
+        print("Error, please try again! ")
+        loginoffline()
     
 loginoffline()
